983a6be6533d092957e7075cc3fc1427a1c4bddb
Nick Spinale <spinalen@carleton.edu>
2015-10-18 21:55:00 -0500

    added shell.nix

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # type-ops
-Various operations for ghc's new type-level naturals
+
+Various operations for ghc's new type-level naturals (in `GHC.TypeLits`).
+
+For example, `QuotRem a b q r` being populated is analogous to `quotRem a b = (q, r)`
+
+This package will expand as other projects need type-level natural operations not provided by `base`.
---
+++ shell.nix
@@ -0,0 +1,25 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, stdenv }:
+      mkDerivation {
+        pname = "type-ops";
+        version = "0.1.0.0";
+        src = ./.;
+        libraryHaskellDepends = [ base ];
+        homepage = "https://github.com/nickspinale/type-ops";
+        license = stdenv.lib.licenses.mit;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
